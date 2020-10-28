34be3d49761e91dbe24aac42f09bfebd5e8a8daf
nicormrz <nicormrz@github.com>
2020-10-28 14:21:46 -0700

    no cursor

--- Interval Timer/Interval Timer/main.cpp
+++ Interval Timer/Interval Timer/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOverrideCursor( QCursor( Qt::BlankCursor ) );
+
     MainWindow w;
     //w.show();
     w.showFullScreen();
